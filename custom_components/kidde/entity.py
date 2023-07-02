d9f06e664003c68926e16329a08e781bb6d66f3e
Sean McGovern <410sean@gmail.com>
2023-07-02 11:31:20 -0400

    Update entity.py

--- custom_components/kidde/entity.py
+++ custom_components/kidde/entity.py
@@ -5,15 +5,15 @@ from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import ATTRIBUTION, DOMAIN, NAME, VERSION
-from .coordinator import BlueprintDataUpdateCoordinator
+from .coordinator import KiddeDataUpdateCoordinator
 
 
-class IntegrationBlueprintEntity(CoordinatorEntity):
-    """BlueprintEntity class."""
+class IntegrationKiddeEntity(CoordinatorEntity):
+    """KiddeEntity class."""
 
     _attr_attribution = ATTRIBUTION
 
-    def __init__(self, coordinator: BlueprintDataUpdateCoordinator) -> None:
+    def __init__(self, coordinator: KiddeDataUpdateCoordinator) -> None:
         """Initialize."""
         super().__init__(coordinator)
         self._attr_unique_id = coordinator.config_entry.entry_id
